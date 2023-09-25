eb766d1331a29f5297b6941cd7581eda5a8563a4
oluceps <ospp@oluceps.uk>
2023-09-25 22:14:50 +0000

    add: clang wasm js binding
    wasm binary file exceeds size limit.
    
    Signed-off-by: oluceps <ospp@oluceps.uk>

--- env.d.ts
+++ env.d.ts
@@ -1,4 +1,5 @@
 /// <reference types="vite/client" />
+/// <reference types="vite-plugin-comlink/client" />
 import 'vite/client'
 declare module 'monaco-editor';
 declare module 'monaco-editor/esm/vs/editor/editor.worker?worker'
