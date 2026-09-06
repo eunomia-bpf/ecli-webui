d5daca50d6f4c97ec4e5f910cda747d09514066c
oluceps <secinia@outlook.com>
2026-09-06 21:03:56 +0800

    style(ui): add syntax highlighting for success, error and warning messages in console

--- src/components/TheConsole.vue
+++ src/components/TheConsole.vue
@@ -6,13 +6,27 @@ const props = defineProps<{
 const formatString = (str: string) => {
 	return str.replace(/\n/g, "<br>");
 };
+
+const getColorClass = (str: string) => {
+    const lower = str.toLowerCase();
+    if (lower.includes('compilation successful')) {
+        return 'text-green-600 font-bold';
+    }
+    if (lower.includes('error') || lower.includes('failed')) {
+        return 'text-red-600';
+    }
+    if (lower.includes('warning')) {
+        return 'text-yellow-600';
+    }
+    return 'text-gray-600';
+};
 </script>
 
 <template>
     <ul class="w-full h-full list-none font-mono m-2">
-        <li v-for="s in props.ctx" :key="s" class="whitespace-nowrap">
-            <span class="text-gray-600" v-if="s.includes('\n')" v-html="formatString(s)"></span>
-            <span class="text-gray-600" v-else>{{ s }}</span>
+        <li v-for="(s, index) in props.ctx" :key="index" class="whitespace-nowrap">
+            <span :class="getColorClass(s)" v-if="s.includes('\n')" v-html="formatString(s)"></span>
+            <span :class="getColorClass(s)" v-else>{{ s }}</span>
         </li>
     </ul>
 </template>
